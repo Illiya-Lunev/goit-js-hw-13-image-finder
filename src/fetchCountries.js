00718d2ebf4a81fce27cedd5714060a92a291b41
Iliya Lunev <lunevig90@gmail.com>
2021-08-16 13:31:17 +0300

    update Parsel

--- src/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-const fetchCountries = searchQuery => {
-  const urlApi = 'https://restcountries.eu/rest/v2/name/';
-  let url = `${urlApi}${searchQuery}`;
-  return fetch(url)
-    .then(res => res.json())
-    .catch(error => console.log('error'));
-};
-export default fetchCountries;
