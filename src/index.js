0008e9c73d4951db7bc4dce8b8c03493b74eaa80
Iliya Lunev <lunevig90@gmail.com>
2021-08-18 20:38:20 +0300

    add basicLightbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ApiService from '../src/apiService';
 import templates from '../src/templates/temlates.hbs';
 import LoadMoreBtn from '../src/load-more-btn';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/src/styles/main.scss';
 
 import { notice, defaults, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
@@ -16,6 +18,7 @@ defaults.width = '300px';
 const ref = {
   formSearch: document.querySelector('.js-search-form'),
   markupGallery: document.querySelector('.js-gallery'),
+  gallery: document.querySelector('.gallery'),
 };
 
 const loadBtnmore = new LoadMoreBtn({
@@ -24,6 +27,7 @@ const loadBtnmore = new LoadMoreBtn({
 });
 const apiService = new ApiService();
 
+ref.gallery.addEventListener('click', openImg);
 ref.formSearch.addEventListener('submit', onSearch);
 loadBtnmore.refs.button.addEventListener('click', onLoadMore);
 
@@ -40,7 +44,6 @@ function onLoadMore() {
   loadBtnmore.disable();
   apiService.fetchArticles().then(onGalleryMarkup);
   loadBtnmore.enable();
-
   scrollAfterLoad();
 
   if (apiService.query) {
@@ -71,4 +74,15 @@ function scrollAfterLoad() {
       block: 'end',
     });
   }, 500);
+}
+
+function openImg(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  basicLightbox
+    .create(
+      `<img class="lightbox__image" src=${e.target.dataset.source} alt="">`,
+    )
+    .show();
 }
